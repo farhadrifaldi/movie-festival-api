0e93612a9d5a22a2045b52b2e09ae72f21cde79a
farhad <farhadrifaldi@gmail.com>
2025-01-20 11:04:48 +0700

    feat: setup connection to postgres using supabase postgres SQL

--- go.mod
+++ go.mod
@@ -7,6 +7,11 @@ toolchain go1.23.5
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.12.7 // indirect
 	github.com/bytedance/sonic/loader v0.2.3 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
@@ -16,6 +21,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.24.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
+	github.com/jackc/pgx/v5 v5.7.2
+	github.com/joho/godotenv v1.5.1
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
+var conn *pgx.Conn
+
 func main() {
+	var err error
+	// Establish a connection to the PostgreSQL database
+	conn, err = pgx.Connect(context.Background(), goDotEnvVariable("POSTGRES_CONNECTION_STRING"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	r := gin.Default()
 
+	// Define first API and make query from connected DB
 	r.GET("/", func(c *gin.Context) {
+		var title string
+		err = conn.QueryRow(context.Background(), "SELECT title from movies WHERE id = $1", 1).Scan(&title)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+
 		c.JSON(200, gin.H{
-			"message": "Hello world",
+			"message": title,
 		})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+/*
+*
+Function to get environment variable from .env
+*/
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot load env")
+		os.Exit(1)
+	}
+
+	return os.Getenv(key)
 }
